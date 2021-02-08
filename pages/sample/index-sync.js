5add1c7b62230c3966cc015b4a5ec84b96de13f9
Dane Bratz <dane@skipperhospitality.com>
2021-02-08 15:31:09 -0800

    initial video

--- pages/sample/index-sync.js
+++ pages/sample/index-sync.js
@@ -1,3 +1,7 @@
 import {bar} from '../../scripts/sample';
+import { lazyLoadVideos } from '../../scripts/lazyloading.js';
 
+document.addEventListener("DOMContentLoaded", lazyLoadVideos('lazy', { 
+    rootMargin: '-100px' // will start loading video with 100px buffer
+ }));
 console.log('BAR BAR BAR jjj BAR', bar)
