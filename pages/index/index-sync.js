5735de938bf00a26b6030a252971c6d892ed58d8
Dane Bratz <dane@skipperhospitality.com>
2021-02-10 14:06:00 -0800

    parameterization

--- pages/index/index-sync.js
+++ pages/index/index-sync.js
@@ -3,5 +3,5 @@
 import { lazyLoadVideos } from '../../scripts/lazyloading.js';
 
 document.addEventListener('DOMContentLoaded', lazyLoadVideos('lazy', {
-    rootMargin: '-100px'
+    rootMargin: '100px'
 }));
