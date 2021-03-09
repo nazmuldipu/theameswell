1ba2f57e92da6d27c0f53912f1aa9c3cd929fd00
Dane Bratz <dane@skipperhospitality.com>
2021-03-09 00:14:51 -0700

    events gallery and video

---
+++ pages/events/events-sync.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import { lazyLoadVideos } from '../../scripts/lazyloading.js';
+
+document.addEventListener('DOMContentLoaded', lazyLoadVideos('lazy', {
+    rootMargin: '100px'
+}));
