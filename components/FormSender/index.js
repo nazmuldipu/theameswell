d07d293548152751a99f9299e6c6460b62b054cd
Dane Bratz <dane@skipperhospitality.com>
2021-03-11 18:04:43 -0800

    form status messaging

--- components/FormSender/index.js
+++ components/FormSender/index.js
@@ -1,37 +1,109 @@
 'use strict';
 
-import { sendContact, isValid } from '../lib/helpers.js'
+import { sendContact, isValid } from '../lib/helpers.js';
+import {
+    STATUS_COLOR_ERROR,
+    STATUS_COLOR_NORMAL,
+    STATUS_COLOR_SUCCESS,
+    SUCCESS_MESSAGE,
+    ERROR_MESSAGE,
+    LOADING_MESSAGE,
+    INVALID_MESSAGE
+} from '../lib/constants.js';
 
 export default class FormSender extends HTMLElement {
+
+    static formInteractionEvents = ['click', 'keydown'];
+
     constructor() {
         super();
-        console.log('FORM SENDER CONSTRUCTOR')
         this.submitButton = this.getElementsByClassName('submit__button')?.[0];
+        this.ajaxStatusEl = this.getElementsByClassName('ajax__status')?.[0];
+        this.setStatusColor(STATUS_COLOR_NORMAL);
         this.form = this.getElementsByTagName('form')?.[0];
+
+        this.formInteractionHandler = (e) => {
+            console.log('form interaction hanlding', e)
+            this.clearFormInteractionListeners();
+            this.hideStatus();
+        };
+
         this.form.onsubmit = (e) => {
             // validate form here -- idealy we combine the iteration of form fields
             // and the iteration of sendContact -- we will refactor later
             e.preventDefault();
             if(isValid(this.form)) {
+                this.showStatus();
+                this.setStatusCopy(LOADING_MESSAGE);
+                this.setStatusColor(STATUS_COLOR_NORMAL);
                 sendContact(this.form, this.dataset.type)
                 .then(res => {
                     if(res.ok) {
-                        //change this to be promise logging
-                        res.json().then(body => console.log('resp body', body))
+                        // then success
+                        res.json().then(body => {
+                            this.setStatusCopy(SUCCESS_MESSAGE);
+                            this.setStatusColor(STATUS_COLOR_SUCCESS);
+                            this.clearForm();
+                        })
                     } else {
                         console.log('res err', res)
+
                         if (res.body instanceof ReadableStream) {
                             // then we have an error object
-                            res.json().then(e => console.log('error obj', e));
+                            res.json().then(e => {
+                                throw e;
+                            });
                         }
                         throw new Error(`SERVER ERROR ${res.status} -- ${res.statusText}`);
                     }
                 })
-                .catch(err => console.error('contact response error', err));
+                .catch(err => {
+                    this.setStatusCopy(ERROR_MESSAGE);
+                    this.setStatusColor(STATUS_COLOR_ERROR);
+                    console.error('contact response error', err)
+                })
+                .finally(() => {
+                    this.applyFormInteractionListeners();
+                });
             } else  {
-                console.log('form invalid');
+                this.setStatusCopy(INVALID_MESSAGE);
+                this.setStatusColor(STATUS_COLOR_ERROR);
+                this.applyFormInteractionListeners();
             }
         };
+    }
+
+    applyFormInteractionListeners() {
+        FormSender.formInteractionEvents.forEach(event => {
+            this.form.addEventListener(event, this.formInteractionHandler, { once: true });
+        });
+    }
+
+    clearFormInteractionListeners() {
+        FormSender.formInteractionEvents.forEach(event => {
+            this.form.removeEventListener(event, this.formInteractionHandler, { once: true })
+        });
+    }
+
+    clearForm() {
+        this.form.reset();
+    }
+
+    setStatusCopy(copy) {
+        this.ajaxStatusEl.textContent = copy;
+    }
+
+    hideStatus() {
+        this.ajaxStatusEl.classList.add('hidden');
+    }
+
+    showStatus() {
+        this.ajaxStatusEl.classList.remove('hidden');
+    }
+
+    setStatusColor(color) {
+        this.ajaxStatusEl.classList.remove(STATUS_COLOR_NORMAL, STATUS_COLOR_SUCCESS, STATUS_COLOR_ERROR);
+        this.ajaxStatusEl.classList.add(color);
     }
 }
 
--- components/lib/constants.js
+++ components/lib/constants.js
@@ -1,2 +1,10 @@
 //export const CONTACT_URL = 'https://bobf6qa3l0.execute-api.us-east-1.amazonaws.com/test/theameswell/contact/';
 export const CONTACT_URL = 'http://localhost:3000/theameswell/contact/';
+
+export const STATUS_COLOR_NORMAL = 'text-ams-primary';
+export const STATUS_COLOR_SUCCESS = 'text-ams-green';
+export const STATUS_COLOR_ERROR = 'text-rose-600';
+export const SUCCESS_MESSAGE = 'Success! You\'ll hear from our team shortly';
+export const LOADING_MESSAGE = 'Contacting our servers...';
+export const ERROR_MESSAGE = 'Something went wrong. Please try again';
+export const INVALID_MESSAGE = 'Please fill in the required fields';
