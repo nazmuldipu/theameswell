862a4608873e143293a15224f6e820632e15f37b
akhost <akhost@gmail.com>
2021-04-12 10:26:28 -0400

    removing  sq. ft.

--- components/lib/ameswell-rooms.js
+++ components/lib/ameswell-rooms.js
@@ -3,84 +3,84 @@
 const roomInfo = [
     {
         title: "Premium King",
-        sqft: "375 sq. ft.",
+        sqft: "375",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     },
     {
         title: "Premium King Accessible",
-        sqft: "440 sq. ft.",
+        sqft: "440",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     },
     {
         title: "Premium 2 Queens",
-        sqft: "395 sq. ft.",
+        sqft: "395",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Premium 2 Queens Accessible",
-        sqft: "485 sq. ft.",
+        sqft: "485",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Junior Suite",
-        sqft: "550 sq. ft.",
+        sqft: "550",
         workspace: "Workspace",
         sleeps: "Sleeps 3"
     },{
         title: "ExecWell Suite",
-        sqft: "690 sq. ft.",
+        sqft: "690",
         workspace: "Workspace",
         sleeps: "Sleeps 3"
     },
     {
         title: "Corner King Suite",
-        sqft: "790 sq. ft.",
+        sqft: "790",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     },
     {
         title: "Arbor Suite",
-        sqft: "779 sq. ft.",
+        sqft: "779",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Ameswell Suite",
-        sqft: "955 sq. ft.",
+        sqft: "955",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Superior King",
-        sqft: "310 sq. ft.",
+        sqft: "310",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     },
     {
         title: "Superior 2 Queens",
-        sqft: "395 sq. ft.",
+        sqft: "395",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Deluxe King",
-        sqft: "335 sq. ft.",
+        sqft: "335",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     },
     {
         title: "Deluxe 2 Queens",
-        sqft: "395 sq. ft.",
+        sqft: "395",
         workspace: "Workspace",
         sleeps: "Sleeps 4"
     },
     {
         title: "Wellness King",
-        sqft: "317 sq. ft.",
+        sqft: "317",
         workspace: "Workspace",
         sleeps: "Sleeps 2"
     }
