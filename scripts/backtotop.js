533e36d6989325e8385215714a792eabf56e576d
Malik Zubayer Ul Haider <xian-malik@outlook.com>
2021-06-22 21:27:10 +0600

    Back to top sticky when section view

--- scripts/backtotop.js
+++ scripts/backtotop.js
@@ -16,4 +16,26 @@ export const backToTop = ( el ) => {
 			behavior: 'smooth' 
 		} );
 	} );
+
+	const pageHeader = document.querySelector('#pageHeader');
+	// const offsetTop = el.offsetTop;
+	const navHeight = pageHeader.offsetHeight;
+
+	var _offset = 0;
+	var _el = el;
+    while( _el && !isNaN( _el.offsetLeft ) && !isNaN( _el.offsetTop ) ) {
+        _offset += _el.offsetTop - _el.scrollTop;
+        _el = _el.offsetParent;
+    }
+
+	function stickyBackToTop() {
+		if (window.scrollY > (_offset - el.offsetTop)) {
+			el.classList.remove('absolute');
+			el.classList.add('fixed');
+		} else {
+			el.classList.add('absolute');
+			el.classList.remove('fixed');
+		}
+	}
+	window.addEventListener('scroll', stickyBackToTop);
 }
