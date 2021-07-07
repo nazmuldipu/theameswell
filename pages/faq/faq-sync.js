75d9422ff1137896587eeffed06c0aa03b6f58b0
Malik Zubayer Ul Haider <xian-malik@outlook.com>
2021-07-07 11:58:10 +0600

    Accordion JS to CSS

--- pages/faq/faq-sync.js
+++ pages/faq/faq-sync.js
@@ -1,11 +1,4 @@
 'use strict';
 import { backToTop } from '../../scripts/backtotop.js';
-import { accordion } from '../../scripts/accordion.js';
 
-document.addEventListener('DOMContentLoaded', function(){
-	// Initialize Accordion script
-	accordion( '.accordion-toggle' );
-
-	// Initialize Back To Top
-	backToTop( '.button--backtotop' );
-});
+document.addEventListener('DOMContentLoaded', backToTop('.button--backtotop'));
