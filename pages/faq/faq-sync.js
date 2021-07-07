7bccd92c7750155c8612c630fd38c5a621b2a609
Malik Zubayer Ul Haider <xian-malik@outlook.com>
2021-07-07 15:33:20 +0600

    #fix

--- pages/faq/faq-sync.js
+++ pages/faq/faq-sync.js
@@ -1,4 +1,4 @@
 'use strict';
 import { backToTop } from '../../scripts/backtotop.js';
 
-document.addEventListener('DOMContentLoaded', backToTop('.button--backtotop'));
+document.addEventListener('DOMContentLoaded', () => backToTop('.button--backtotop'));
