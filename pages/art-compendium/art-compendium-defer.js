fe4611f7498072247250cf3875e3b3284d5336e1
meherab-hossain <h.meherabhossain@gmail.com>
2021-08-06 15:22:06 +0600

    svg arrow toggle added and aligned

--- pages/art-compendium/art-compendium-defer.js
+++ pages/art-compendium/art-compendium-defer.js
@@ -1,8 +1,10 @@
 const readMoreBtn = document.querySelectorAll(".read-more__btn");
 const moreText = document.querySelectorAll(".moreText");
+const toggleArrow=document.querySelectorAll(".toggle-arrow");
 
 for (let i = 0; i < readMoreBtn.length; i++) {
     readMoreBtn[i].addEventListener("click", (e) => {
-        moreText[i].classList.toggle('active') ? readMoreBtn[i].innerText = "READ LESS" : readMoreBtn[i].innerText = "READ MORE";
+        moreText[i].classList.toggle('active') ? readMoreBtn[i].innerText = "LESS" : readMoreBtn[i].innerText = "MORE";
+        toggleArrow[i].classList.toggle('active');
     })
 }
