70695a1649bcdb562cd14b066d4715474e9a0890
Nazmul Alam <nazmuldipu@gmail.com>
2021-08-11 15:21:59 +0600

    background color transition added

--- pages/art-compendium/art-compendium-defer.js
+++ pages/art-compendium/art-compendium-defer.js
@@ -8,3 +8,47 @@ for (let i = 0; i < readMoreBtn.length; i++) {
         toggleArrow[i].classList.toggle('active');
     })
 }
+const trim = (str) => {
+    return str.trim ? str.trim() : str.replace(/^\s+|\s+$/g, '');
+};
+const addClass = (el, cn) => {
+    if (!hasClass(el, cn)) {
+        el.className = (el.className === '') ? cn : el.className + ' ' + cn;
+    }
+}
+const removeClass = (el, cn) => {
+    el.className = trim((' ' + el.className + ' ').replace(' ' + cn + ' ', ' '));
+}
+const hasClass = (el, cn) => {
+    return (' ' + el.className + ' ').indexOf(' ' + cn + ' ') !== -1;
+}
+
+const getYPosition = (el) => {
+    let yPos = 0;
+
+    while (el) {
+        if (el.tagName == "BODY") {
+            let yScroll = el.scrollTop || document.documentElement.scrollTop;
+            yPos += (el.offsetTop - yScroll + el.clientTop);
+        } else {
+            yPos += (el.offsetTop - el.scrollTop + el.clientTop);
+        }
+        el = el.offsetParent;
+    }
+    return yPos;
+}
+
+const artComponent = document.querySelector('#artComponent');
+let currentBg = '';
+document.addEventListener('scroll', function (e) {
+    const panels = document.querySelectorAll('.panel');
+    panels.forEach(el => {
+        const yPos = getYPosition(el);
+        if (yPos < 300 && yPos + el.offsetHeight > 300) {
+            const dataColor = el.getAttribute('data-color')
+            removeClass(artComponent, currentBg);
+            addClass(artComponent, dataColor);
+            currentBg = dataColor;
+        }
+    })
+});
