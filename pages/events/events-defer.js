6d8db139a3aeddaa4fad6d22b9e0b1bd04ba89b3
meherab-hossain <h.meherabhossain@gmail.com>
2021-09-20 11:23:04 +0600

    console removed

--- pages/events/events-defer.js
+++ pages/events/events-defer.js
@@ -1,6 +1,5 @@
 const showAllBtn = document.querySelector('.show-all__amenities');
 const eventsSvgIcon = document.querySelectorAll('.events__svgicon');
-console.log(eventsSvgIcon.length);
 showAllBtn.addEventListener("click", (e)=>{
     for (let index = 0; index < eventsSvgIcon.length; index++) {
         eventsSvgIcon[index].classList.add('grid');
