2408db8cf0b69de807f65e4a00454fbffb190bcc
dane-skipper <72758450+dane-skipper@users.noreply.github.com>
2021-12-07 15:26:54 -0800

    fixes #1096 (#1097)

--- pages/rfp/rfp-module.js
+++ pages/rfp/rfp-module.js
@@ -2,15 +2,20 @@
 import '../../components/FormSender/index.js';
 import '../../components/WeatherWidget/index.js';
 
-var infoBox = document.getElementById('foodBeverageYesText');
+const infoBox = document.getElementById('foodBeverageYesText');
 infoBox.style.display = 'none';
 
-var radios = document.querySelectorAll('input[name="foodBeverage"]');
+const foodBeverageItemsSelect = document.getElementById('foodBeverageItemsSelect');
+foodBeverageItemsSelect.required = "";
+
+const radios = document.querySelectorAll('input[name="foodBeverage"]');
 function changeHandler(event) {
    if ( this.value === 'yes' ) {
       infoBox.style.display = 'block';
+      foodBeverageItemsSelect.required = "required";
    } else if ( this.value === 'no' ) {
       infoBox.style.display = 'none';
+      foodBeverageItemsSelect.required = "";
    }  
 }
 
